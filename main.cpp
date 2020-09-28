c1511be6f683dac5f686dbbe89df22fa94068323
Haled Abdul <haled.abdulhakimnaser7475@alumnos.udg.mx>
2020-09-28 17:39:02 -0500

    Actividad realizada

--- main.cpp
+++ main.cpp
@@ -3,7 +3,34 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+
+    string cadena;
+    cout << endl << "Tipo de dato: ";
+    getline(cin, cadena);
+    if(cadena=="int"){
+        cout << endl << sizeof(int);
+
+    }else if(cadena == "char"){
+        cout << endl << sizeof(char);
+
+    }else if(cadena== "long"){
+        cout << endl << sizeof(long);
+        
+    }
+    else if(cadena== "float"){
+        cout << endl << sizeof(float);
+
+    }
+    else if(cadena== "short"){
+        cout << endl << sizeof(short);
+
+    }
+    else if(cadena== "double"){
+        cout << endl << sizeof(double);
+
+    }else{
+        cout << "Dato erroneo";
+    }
 
     return 0;
 }
